cd2116ad7c234c3c8a7a2cc54cb3d645fb7baf4b
iagokrt <iago_floripa@hotmail.com>
2021-09-29 01:46:19 -0300

    experiments on about component r3f drei shader distortion material

--- src/components/blog/About.js
+++ src/components/blog/About.js
@@ -10,19 +10,31 @@ const About = () => {
     return (
         <>
             <aside className="about-me-container">
-                <div className="canvas-overlapsed-dom" style={{position: 'absolute'}}>
+                <div className="canvas-overlapsed-dom" style={{position: 'absolute', width: '90%'}}>
                     <p className="">
                        I'm Learning about designing things. 
+                    </p>
+                    <p className="">
+                        I'm a programmer that learn about computer graphics too early in life. <br />
+                        So when I started to code, every project I designed was related to some kinda of artistic background.
+                    </p>
+                    <p className="">
+                        Now that you're here at Write  Island, you can finally find contents related to: <br />
+                        Coding, web development, html, css, three.js, glsl, coreldraw, reactjs, three-fiber, react native & node.js. 
                     </p>
                     <p className="">
                         Now that you're here at Write  Island, you can finally find contents related to: <br />
                         Coding, web development, html, css, three.js, glsl, coreldraw, reactjs, three-fiber, react native & node.js. 
                     </p>
                 </div>
-                <Canvas concurrent colorManagement>
+                <Canvas concurrent colorManagement style={{width: '90%'}}>
 
                     <group>
-                        <ambientLight intensity={0.1} />
+                        <ambientLight intensity={0.5} />
+
+                        <directionalLight color="yellow" position={[0, 0, ]} />
+
+
                         <directionalLight color="blue" position={[0, 0, 5]} />
                         <directionalLight color="blue" position={[0, 2, 0]} />
                         <directionalLight color="blue" position={[0, 0, -5]} />
@@ -35,24 +47,28 @@ const About = () => {
 
                     <group>
                         <mesh position={[0,0, 36]}>
-                            <sphereBufferGeometry args={[210, 290, 95]} attach="geometry" />
+                            <sphereBufferGeometry 
+                                args={[15, 2, 55]}
+                                attach="geometry"
+                            />
                             <MeshWobbleMaterial
                                 attach="material"
                                 side={FrontSide}
-                                metalness={0.55}
-                                roughness={0.24}
-                                speed={0.8}
-                                factor={1}
-                                color={"#890022"}
+                                metalness={0.42}
+                                speed={Math.PI / 2}
+                                color={"#0192ec"}
                             />
                         </mesh>
                     </group>
                         
-                    <OrbitControls minAzimuthAngle={Math.PI*2} maxAzimuthAngle={Math.PI}  />
+                    <OrbitControls enableZoom={false} enableDamping={true} />
                 </Canvas>
             </aside>
         </>
     )
 }
-
+/**
+ *   <OrbitControls minAzimuthAngle={Math.PI*2} maxAzimuthAngle={Math.PI}  />
+ * 
+ */
 export default About
