5ec932cead37944f592a084686dadb458c51282c
Iago Barreto <iago_floripa@hotmail.com>
2025-08-01 03:01:58 -0300

    add 2025 post yea

---
+++ content/blog/normalized-background-changers.md
@@ -0,0 +1,17 @@
+---
+title: normal-comparisons
+date: "2025-08-01T02:12:03.284Z"
+description: "Powershell"
+---
+
+## Nesse post vou falar um pouquinho sobre 2 principais coisas:
+
+- trocar planos de fundo
+
+- noise functions
+
+---
+
+Esse tema não se correlaciona intencionalmente e usar powershell para fazer isso nós iremos... nd ve mane, segue o repo:
+
+    https://github.com/Fsocietyybr/normal-comparisons
--- src/state/technologies.js
+++ src/state/technologies.js
@@ -16,26 +16,32 @@ const technologies = [
         source: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png',
         alt: 'JAVASCRIPT'
     },
-    {
-        title: 'TYPESCRIPT',
-        source: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png',
-        alt: 'TYPESCRIPT'
-    },
+   
     {
         title: 'NODEJS',
         source: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png',
         alt: 'NODEJS'
     },
     {
-        title: 'REACT',
-        source: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png',
-        alt: 'REACT'
+        title: 'PHP',
+        source: "https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white",
+        alt: 'PHP'
     },
     {
-        title: 'THREEJS',
-        source: ThreeBadge,
-        alt: 'THREEJS'
-    }
+        title: 'BITBUCKET',
+        source: 'https://user-images.githubusercontent.com/25181517/192108375-268c35e6-ab26-44b2-88bf-e3121a4e5083.png',
+        alt: 'BITBUCKET'
+    },
+    {
+        title: 'GIT',
+        source: 'https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png',
+        alt: 'GIT'
+    },
+    {
+        title: 'ANGULAR',
+        source: 'https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white"',
+        alt: 'ANGULAR'
+    },
 ]
 
 export default technologies;
